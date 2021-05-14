cbbce9951f3313ba02ad33d90351af276df35d70
Alexei Pancratov <drivergear@gmail.com>
2021-05-14 12:02:36 -0400

    Fixed additional conflict

--- src/App.js
+++ src/App.js
@@ -1,9 +1,5 @@
 import "./App.css";
 import BooksList from "./components/BooksList";
-<<<<<<< HEAD
-=======
-
->>>>>>> 71fb07e85f34a54d11789b57dbaebef61095909c
 import BookDetails from "./components/BookDetails";
 import { Switch, Route, BrowserRouter } from "react-router-dom";
 
@@ -16,11 +12,7 @@ function App() {
           <div className="col-md">
             <Switch>
               <Route path="/" component={BooksList} exact/>
-<<<<<<< HEAD
               <Route path="/books/:id" component={BookDetails} />
-=======
-              <Route path="/books/:id" element={BookDetails} />
->>>>>>> 71fb07e85f34a54d11789b57dbaebef61095909c
             </Switch>
           </div>
         </div>
